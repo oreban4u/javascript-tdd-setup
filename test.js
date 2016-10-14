a15637e7df6a8cdc3884aba49c448ab67d2217c6
Oreoluwa Banwo <oreban4u@yahoo.co.uk>
2016-10-14 18:00:19 +0100

    Added 3 Test Cases

---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var chai = require('chai');
+var assert = chai.assert;
+
+var lib = require('./lib/sum_of_primes.js');
+
+describe("Test that sum of primes works", function() {
+  it("should give 17 for the input 8", function() {
+    assert(lib.sumOfPrimes(8) == 17);
+    }
+
+  it("should give 28 for the input 11", function(){
+    assert(lib.sumOfPrimes(11) == 28);
+
+  }
+  it("should give 5 for the input 3", function(){
+    assert(lib.sumOfPrimes(3) == 5);
+
+  }
+
+}
+
