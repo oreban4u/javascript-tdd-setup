bbf9592d0709eec7c1b6345ddadd6af64d2b42dd
Oreoluwa Banwo <oreban4u@yahoo.co.uk>
2016-10-15 13:47:36 +0100

    All Tests Sussessfully passed

--- test.js
+++ test.js
@@ -6,45 +6,45 @@ var assert = chai.assert;
 var lib = require('./lib/sum_of_primes.js');
 
 describe("Test that sum of primes works", function() {
-  it("should give 17 for the input 8", function() {
+  it("should return 17 for the input 8", function() {
     assert(lib.sumOfPrimes(8) == 17);
-    }
+  });
 
-  it("should give 28 for the input 11", function(){
+  it("should return 28 for the input 11", function(){
     assert(lib.sumOfPrimes(11) == 28);
 
-  }
-  it("should give 5 for the input 3", function(){
+  });
+  it("should return 5 for the input 3", function(){
     assert(lib.sumOfPrimes(3) == 5);
 
-  }
-  it("should give 2 for the input 2", function(){
+  });
+  it("should return 2 for the input 2", function(){
     assert(lib.sumOfPrimes(2) == 2);
 
-  }
-  it("should give 5 for the input 4", function(){
-    assert(lib.sumOfPrimes(4) == 5);
+  });
+  it("should return 'Invalid input' for the input {}", function(){
+    assert(lib.sumOfPrimes({}) == "Invalid Input");
 
-  }
-  it("should give 17 for the input 7", function(){
-    assert(lib.sumOfPrimes(7) == 17);
+  });
+  it("should return 'Invalid Input' for array input", function(){
+    assert(lib.sumOfPrimes([4,5]) == "Invalid Input");
 
-  }
-  it("should give 10 for the input 5", function(){
-    assert(lib.sumOfPrimes(-5) == false);
+  });
+  it("should return 'Invalid Input' for the input -5", function(){
+    assert(lib.sumOfPrimes(-5) == 'Invalid Input');
 
-  }
-  it("should give 41 for the input 13", function(){
-    assert(lib.sumOfPrimes('') == false);
+  });
+  it("should return 'Invalid Input' for string input", function(){
+    assert(lib.sumOfPrimes('') == 'Invalid Input');
 
-  }
-  it("should give 0 for the input 0", function(){
-    assert(lib.sumOfPrimes(0) == false);
+  });
+  it("should return 'Invalid Input' for the input 0", function(){
+    assert(lib.sumOfPrimes(0) == 'Invalid Input');
 
-  }
-  it("should give 0 for the input 1", function(){
-    assert(lib.sumOfPrimes(1) == false);
+  });
+  it("should return 'Invalid Input' for the input 1", function(){
+    assert(lib.sumOfPrimes(1) == 'Invalid Input');
 
-  }
-}
+  });
+});
 
